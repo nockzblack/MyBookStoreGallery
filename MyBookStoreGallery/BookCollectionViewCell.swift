5b997b3f7b09a79d577b827e0fd6b399a574d962
Fernando Benavides <nockzblack@gmail.com>
2022-04-02 14:58:19 -0600

    Cell class to populate image with the book cover

---
+++ MyBookStoreGallery/BookCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookCollectionViewCell.swift
+//  MyBookStoreGallery
+//
+//  Created by Fernando's Mac on 01/04/22.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookCoverImageView: UIImageView!
+    
+    
+}
